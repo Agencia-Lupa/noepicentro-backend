bce9e0d614789d91079d353016e3edd59e41d29b
RodrigoMenegat <rodrigoschuinski@gmail.com>
2020-06-14 02:54:09 -0300

    Adds .py scripts that can be run from the terminal

---
+++ code/find_radius.py
@@ -0,0 +1,401 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[30]:
+
+
+import multiprocessing
+from geofeather import to_geofeather, from_geofeather
+from shapely.geometry import Point, Polygon
+import pandas as pd
+import geopandas as gpd
+import glob, random, sys, time
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+pd.options.mode.chained_assignment = None  # default='warn'
+gpd.options.use_pygeos = True
+
+
+# In[49]:
+
+
+def read_data(path_to_tracts, path_to_shp):
+    
+    dtype = { 
+        
+        "CD_GEOCODI": str,
+        "CD_GEOCODM": str,
+        "CD_MUNICIP": str,
+        "Cod_setor": str
+        
+    }
+    
+    tracts = pd.read_csv(path_to_tracts, dtype=dtype)
+    
+    shp = gpd.read_file(path_to_shp, dtype=dtype)
+    
+    return tracts, shp
+
+
+# In[31]:
+
+
+def random_points(n=1):
+    
+    '''
+    Generates n random points in the country
+    '''
+    
+    # Load the outline of Brazil
+    polygon = gpd.read_file("https://servicodados.ibge.gov.br/api/v2/malhas?qualidade=1&formato=application/vnd.geo+json")
+    
+    polygon = polygon.loc[0, 'geometry']
+        
+    # Get's its bounding box
+    minx, miny, maxx, maxy = polygon.bounds
+    
+    # Generates a random point within the bounding box
+    # untill it falls within the country
+    
+    points = [ ]
+    
+    while len(points) < n:
+        
+        point = Point(random.uniform(minx, maxx), random.uniform(miny, maxy))
+        
+        if polygon.contains(point):
+            
+            points.append(point)
+            
+    return points
+
+
+# In[46]:
+
+
+def parse_input(argv):
+    
+    '''
+    Parses the input that was passad 
+    through the command line and returns
+    a point
+    '''
+                
+    point = [float(coord.strip()) for coord in argv]
+    
+    point = Point(point[1], point[0]) # Shapely requires a lon, lat point
+        
+    return point
+
+
+# In[47]:
+
+
+def covid_count(measure='deaths'):
+    '''
+    TO DO
+    
+    Returns the current number of covid deaths
+    or cases registered in the Country
+    '''
+    
+    return 100000
+
+
+# In[48]:
+
+
+def find_user_area(point, target):
+    
+    '''
+    Finds the area that we will need to
+    process according to the position of the point
+    '''
+    
+    # A list with the quadrants whose population should be counted
+    
+    quadrants_to_count = [ ]
+    
+    # A list that will be filled with the ids of the quadrants that we need to load
+    # That is, the quadrants to count plust its neighbors
+    
+    quadrants_to_load = [ ]
+    
+    # Loads the quadrant data
+    
+    reference_map = from_geofeather("../data/index_bboxes.feather")
+       
+    # Finds in which quadrant the point falls
+    
+    user_area = reference_map[ reference_map.geometry.contains(point) ].reset_index(drop=True)
+        
+    assert user_area.shape[0] == 1
+        
+    # At first, we will count the population in that particular quadrant
+        
+    quadrants_to_count.append(user_area.loc[0, 'id_no'])
+    
+    # And add itself and its neighbors to those we should load
+    quadrants_to_load.append(user_area.loc[0, 'id_no'])
+    
+    quadrants_to_load.extend(user_area.loc[0, 'neighbors'].split("|"))
+
+    # Checks if the population is enough. If not, add more quadrants
+        
+    population_in_area = reference_map[ reference_map.id_no.isin(quadrants_to_count)].total_population.sum()
+    
+    while population_in_area < target:
+                                 
+        # Adds the neighbors of all counted quadrants to those we should count
+        
+        for index, row in reference_map[ reference_map.id_no.isin(quadrants_to_count)].iterrows():
+                
+            quadrants_to_count.extend(row.neighbors.split("|"))
+                                      
+        quadrants_to_count = list(set(quadrants_to_count))
+                                    
+        
+        # Adds the neighbors of the loaded neighbors to those we should load
+        
+        for index, row in reference_map[ reference_map.id_no.isin(quadrants_to_load)].iterrows():
+                                     
+            quadrants_to_load.extend(row.neighbors.split("|"))
+                                    
+        quadrants_to_load = list(set(quadrants_to_load))
+        
+        # Gets the new population
+        
+        population_in_area = reference_map[ reference_map.id_no.isin(quadrants_to_count)].total_population.sum()
+    
+    # Loads the data in
+    
+    gdfs = [ ]
+    
+    quadrants = reference_map [reference_map.id_no.astype(str).isin(quadrants_to_load) ]
+    
+    for index, row in quadrants.iterrows():
+        
+        fpath = row.fpath
+        
+        gdf = from_geofeather(fpath)
+        
+        gdfs.append(gdf)
+        
+    return pd.concat(gdfs)
+
+
+# In[50]:
+
+
+def merge_tracts_and_shape(tracts, shp):
+    
+    return shp.merge(tracts, left_on='CD_GEOCODI', right_on='Cod_setor', how='left')
+
+
+# In[118]:
+
+
+def find_radius(point, tracts, spatial_index, target):
+    
+    ########################
+    ### HELPER FUNCTIONS ###
+    ########################
+    
+    def find_intersections(tracts, spatial_index, area):
+        '''
+        Finds all the polygons that intersect a given radius
+        '''
+        
+        # Uses Geopandas/PyGeos rtree to pre-filter the tracts
+        nearby_index = list(spatial_index.intersection(area.bounds))
+        
+        nearby_tracts = tracts.iloc[nearby_index]
+        
+        # Selects the tracts that do intersect with the area
+        matches = nearby_tracts [ nearby_tracts.geometry.intersects(area)]
+        
+        return matches
+        
+                    
+    def compute_population_in_area(matches, area):
+        '''
+        Calculates how many people live in the intersecting polygons.
+        Also returns an array with the intersecting shapes.
+        '''
+
+        def process_intersection(population, tract, polygon):
+
+            intersection = tract.intersection(polygon)
+
+            intersection_percentage = intersection.area / tract.area 
+
+            population_in_intersection = population * intersection_percentage
+
+            return intersection, population_in_intersection
+
+        intersection, population_in_intersection = process_intersection(matches.populacao_residente.values,
+                                         matches.geometry.values,
+                                         area)
+
+        matches['geometry'] = intersection
+        
+        matches['population_in_intersection'] = population_in_intersection
+
+        return matches
+    
+    #################
+    ### EXECUTION ###
+    #################
+    
+    checkpoint = time.time()
+    
+    total_people = 0
+    
+    radius = .01 # This unit is lat/lon degrees
+    
+    checkpoint_b = time.time()
+    
+    # While we don't meet the population target, we keep increasing the radius to grab more people
+    while True:
+                
+        total_people = 0
+        
+        area = point.buffer(radius)
+        
+        matches = find_intersections(tracts, spatial_index, area)
+        
+        matches = compute_population_in_area(matches, area)
+        
+        total_people = round(matches.population_in_intersection.sum())
+                        
+        if total_people < target:
+        
+            radius = radius * 1.5
+            
+            continue
+        
+        # Else, finish the iteration
+        else:
+            
+            break
+            
+            
+    # Now we can move into the fine-tuning, removing excess population
+    
+    checkpoint_b = time.time()
+    
+    direction = 'shrink'
+    
+    fine_tune = .5
+    
+    max_tolerance = target * 1.1
+    
+    min_tolerance = target * 0.9
+    
+    while True:
+                        
+        if total_people > max_tolerance:
+            
+            new_direction = 'shrink'
+    
+            total_people = 0
+            
+            radius = radius * (1 - fine_tune)
+            
+            area = point.buffer(radius)
+            
+            matches = find_intersections(tracts, spatial_index, area)
+        
+            matches = compute_population_in_area(matches, area)
+
+            total_people = round(matches.population_in_intersection.sum())
+                        
+            if total_people <= max_tolerance and total_people >= min_tolerance:
+                
+                break
+                
+        
+        elif total_people < min_tolerance:
+            
+            new_direction = 'grow'
+            
+            total_people = 0
+            
+            radius = radius * (1 + fine_tune)
+            
+            area = point.buffer(radius)
+            
+            matches = find_intersections(tracts, spatial_index, area)
+        
+            matches = compute_population_in_area(matches, area)
+
+            total_people = round(matches.population_in_intersection.sum())
+            
+            
+            if total_people <= max_tolerance and total_people >= min_tolerance:
+                
+                break
+                
+        else: # It's equal
+            
+            break
+                
+        if new_direction != direction:
+                        
+            direction = new_direction
+    
+            fine_tune = fine_tune / 2
+     
+        
+    matches = matches[["CD_GEOCODI", "geometry", "population_in_intersection"]]
+    
+    # to_geofeather(matches, f"../output/radiuses/{point}.feather")
+    
+    return matches, area
+
+
+# In[140]:
+
+
+def process_input(point):
+    
+    point = parse_input(point)
+        
+    # TO DO: discover how many covid-19 deaths we have at this point in time
+    target = covid_count()
+            
+    # Gets the parts of the census tracts with the user data that we need to load
+    gdf = find_user_area(point, target)
+        
+    # Uses a buffer to avoid self-intercepting shapes
+    gdf["geometry"] = gdf.geometry.buffer(0)
+        
+    # Creates a sindex to improve search
+    spatial_index = gdf.sindex
+        
+    # Finds the area that we will need to highlight along with the respective population
+    matches, area = find_radius(point, gdf, spatial_index, target)
+
+    # TO DO: save output as geofeather
+    pass
+
+    # Returns the point and it's respective radius as output
+    # TO DO: for test purposes with Tiago only
+    return point.coords[0], area.exterior.coords[0]
+
+
+# In[141]:
+
+
+def main(argv):
+    
+    argv = sys.argv
+    
+    if len(argv) != 2:
+        print("Usage: python lat lon")
+        sys.exit(1)
+    
+    # Gets input from user and turns it into a shapely point
+    return process_input(argv)
+    
+
---
+++ code/prepare_tracts.py
@@ -0,0 +1,294 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+'''
+In this script, we split the spaefiles in a 150 x 150 grid, allowing
+for better performance when computing the affected area in the main
+script
+'''
+
+
+# In[2]:
+
+
+from geofeather import to_geofeather, from_geofeather
+from shapely.geometry import Polygon, MultiPolygon, LineString
+from shapely.ops import split
+from sys import getsizeof
+import geopandas as gpd
+import pandas as pd
+import glob, multiprocessing, os, re, shutil
+
+gpd.options.use_pygeos = True
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+
+
+# In[3]:
+
+
+def read_data(path_to_tracts, path_to_shp):
+    
+    '''
+    Reads both the demographic information
+    and the geographic outline of the census
+    tracts
+    '''
+    
+    dtype = { 
+        
+        "CD_GEOCODI": str,
+        "CD_GEOCODM": str,
+        "CD_MUNICIP": str,
+        "Cod_setor": str
+        
+    }
+    
+    tracts = pd.read_csv(path_to_tracts, dtype=dtype)
+    
+    shp = gpd.read_file(path_to_shp, dtype=dtype)
+    
+    return tracts, shp
+
+
+# In[4]:
+
+
+def merge_tracts_and_shape(tracts, shp):
+    
+    '''
+    Joins the demographic information
+    with the correspoding geographic outline.
+    Returns a geodataframe.
+    '''
+    
+    return shp.merge(tracts, left_on='CD_GEOCODI', right_on='Cod_setor', how='left')
+
+
+# In[5]:
+
+
+def divide_bbox(rectangle, nrows, ncols): 
+    '''
+    Divides a rectangular bounding box in
+    rows and columns
+
+    Reference: https://stackoverflow.com/questions/58283684/how-to-divide-a-rectangle-in-specific-number-of-rows-and-columns
+    '''
+
+    minx, miny, maxx, maxy = rectangle.bounds
+
+    dx = (maxx - minx) / nrows  # width of a small part
+
+    dy = (maxy - miny) / ncols  # height of a small part
+
+    horizontal_splitters = [LineString([(minx, miny + i*dy), (maxx, miny + i*dy)]) for i in range(ncols)]
+
+    vertical_splitters = [LineString([(minx + i*dx, miny), (minx + i*dx, maxy)]) for i in range(nrows)]
+
+    splitters = horizontal_splitters + vertical_splitters
+
+    for splitter in splitters:
+        rectangle = MultiPolygon(split(rectangle, splitter))
+
+    return [ split_rectangle for split_rectangle in rectangle ]
+
+
+# In[6]:
+
+
+def find_neighbors(row, gdf):
+    '''
+    Finds all the polygons in the GeoDataFrame
+    that are neighbors to the current row
+    '''
+
+    neighbors = gdf [ ~gdf.geometry.disjoint(row.geometry)].id_no.astype(str).tolist() 
+    
+    neighbor_count = len(neighbors)
+    
+    neighbors = "|".join(neighbors)
+
+    return pd.Series({
+        "neighbors": neighbors,
+        "neighbor_count": neighbor_count
+    })
+
+
+# In[7]:
+
+
+def find_intersections(tracts, spatial_index, area):
+    '''
+    Finds all the polygons that intersect a given area
+    '''
+    
+    # Uses Geopandas/PyGeos rtree to pre-filter the tracts
+    nearby_index = list(spatial_index.intersection(area.bounds))
+    
+    nearby_tracts = tracts.iloc[nearby_index]
+
+    # Selects the tracts that do intersect with the area
+    matches = nearby_tracts [ nearby_tracts.geometry.intersects(area)]
+
+    return matches
+
+
+# In[8]:
+
+
+def compute_population_in_area(matches, area):
+    '''
+    Calculates how many people live in the intersecting polygons.
+    Also returns an array with the intersecting shapes.
+    '''
+
+    def process_intersection(population, tract, polygon):
+
+        intersection = tract.intersection(polygon)
+
+        intersection_percentage = intersection.area / tract.area 
+
+        population_in_intersection = population * intersection_percentage
+
+        return intersection, intersection_percentage, population_in_intersection
+    
+    
+
+    intersection, intersection_percentage, population_in_intersection = process_intersection(matches.populacao_residente.values,
+                                     matches.geometry.values,
+                                     area)
+
+    matches['geometry'] = intersection
+    
+    matches['INTERSECT'] = intersection_percentage.round(2)
+
+    matches['POP_INTER'] = population_in_intersection.round()
+
+    return matches.reset_index(drop=True)
+
+
+# In[9]:
+
+
+def split_tracts(row, output_dir, sindex, tracts):
+    '''
+    Splits the census tracts in equally sized bounding boxes, 
+    and saves each one to a .feather file to avoid a huge load time 
+    when doing the proper data processing. Note that this function
+    does alter the input geometries and their populations according
+    to the interception with the bounding box.
+    '''
+
+    def f(bbox, index, output_dir, sindex, tracts):
+        '''
+        The df.apply function above is simply a wrapper for this
+        function, which does the work item by item
+        '''
+
+        # Finds the intersecting tracts and do the relevant computations
+
+        matches = find_intersections(tracts, sindex, bbox)
+
+        matches = compute_population_in_area(matches, bbox)
+
+        # Adds relevant information to the bboxes dataframe
+
+        total_population = matches.POP_INTER.sum()     
+
+        fname =  f'bbox-{index}.feather'
+
+        fpath = output_dir + fname
+        
+        # If relevant, saves
+        if matches.shape[0] != 0:
+            
+            to_geofeather(matches, fpath)
+            
+        return pd.Series({
+            "fpath": fpath,
+            "total_population": total_population
+        })
+                
+    #################
+    ### EXECUTION ###
+    #################
+       
+    bbox = row.geometry
+    
+    index = row.name
+    
+    return f(bbox, index, output_dir, sindex, tracts)
+
+
+# In[10]:
+
+
+def main():    
+        
+    df, gdf = read_data("../data/censo_dados_resumidos.csv","../data/setores_censitarios_shp_reduzido/")
+    
+    gdf = merge_tracts_and_shape(df, gdf)    
+    
+    gdf.geometry = gdf.geometry.buffer(0)
+        
+    sindex = gdf.sindex
+        
+    brazil_bbox = Polygon([
+        [-74.3143068749,-34.2970741167],
+        [-34.4119631249,-34.2970741167],
+        [-34.4119631249,5.648611595],
+        [-74.3143068749,5.648611595],
+        [-74.3143068749,-34.2970741167]
+    ])
+    
+    
+    bboxes = divide_bbox(brazil_bbox, 150, 150)
+    
+    bboxes = gpd.GeoDataFrame(geometry=bboxes).reset_index().rename(columns={'index':'id_no'})
+    
+    bboxes.crs = gdf.crs
+            
+    # Make sure that the output directory is empty, avoiding overwrites
+    
+    directory =  "../data/setores_censitarios_divididos_feather/"
+            
+    for f in glob.glob(directory + "*"):
+        
+        os.remove(f)
+    
+    # Splits the tracts in bboxes, extracting the relevant information
+    # Note that the function also saves files to the directory and
+    # adjusts the population according to the intersections
+
+    new_data = bboxes.apply(split_tracts, args=[directory, sindex, gdf], axis=1)
+        
+    bboxes['fpath'] = new_data["fpath"]
+    
+    bboxes['total_population'] = new_data["total_population"]
+        
+    # Remove from the data table all the bounding boxes that contain no tracts
+    
+    saved_files = glob.glob(directory + "*.feather")
+        
+    meaningful_bboxes = [ int(re.search('bbox\-(\d+)\.feather', file).group(1)) for file in saved_files ]
+    
+    bboxes = bboxes.loc[meaningful_bboxes].reset_index(drop=True)
+
+    # Finds the neighbors and counts
+    
+    bboxes[['neighbors', 'neighbor_count']] = bboxes.apply(find_neighbors, args=[bboxes], axis=1)    
+
+    to_geofeather(bboxes, "../data/index_bboxes.feather")
+    
+    return bboxes
+
+
+# In[11]:
+
+
+if __name__ == "__main__":
+    main()
+
