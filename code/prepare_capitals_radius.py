30238534f8166a5ca4999f1e943b7cb6b1891ca3
RodrigoMenegat <rodrigoschuinski@gmail.com>
2020-07-05 06:20:51 -0300

    Extends capitals list

--- code/prepare_capitals_radius.py
+++ code/prepare_capitals_radius.py
@@ -49,6 +49,8 @@ def main():
 
     capitals_data = [ 
 
+        # SUL
+
         {
 
             "code_muni": "431490", # First six IBGE digits
@@ -59,9 +61,30 @@ def main():
 
             "display_text": "da Usina do Gasômetro",
 
+            "complement": "ponto turístico da capital gaúcha",
+
             "input_point": ["-30.0341319", "-51.2432707"]
 
         },
+
+        {
+
+            "code_muni": "410690",
+
+            "name_muni": "Curitiba",
+
+            "name_state": "PR",
+
+            "display_text": "do Passeio Público",
+
+            "complement": "parque no centro da cidade",
+
+            "input_point": ["-25.4251957", "-49.2675855"]
+
+        },
+
+        # SUDESTE
+
 
         {
 
@@ -72,6 +95,8 @@ def main():
             "name_state": "SP",
 
             "display_text": "do MASP",
+
+            "complement": "museu mais famoso da metrópole",
 
             "input_point": ["-23.5628876", "-46.6504141"]
 
@@ -87,7 +112,112 @@ def main():
 
             "display_text": "do Maracanã",
 
+            "complement": "maior estádio do país",
+
             "input_point": ["-22.9120302", "-43.2319878"]
+
+        },
+
+
+        # CENTRO-OESTE
+
+        {
+
+            "code_muni": "530010",
+
+            "name_muni": "Brasília",
+
+            "name_state": "DF",
+
+            "display_text": "da Catedral Metropolitana",
+
+            "complement": "projetada por Oscar Niemeyer",
+
+            "input_point": ["-15.7986852", "-47.8757942"]
+
+        },
+
+        {
+
+            "code_muni": "260620",
+
+            "name_muni": "Goiânia",
+
+            "name_state": "GO",
+
+            "display_text": "Monumento às Três Raças",
+
+            "complement": "no setor central do município",
+
+            "input_point": ["-16.6798512", "-49.2558648"]
+
+        },
+
+        # NORDESTE
+
+        {
+
+            "code_muni": "292740",
+
+            "name_muni": "Salvador",
+
+            "name_state": "BA",
+
+            "display_text": "Elevador Lacerda",
+
+            "complement": "cartão postal da região",
+
+            "input_point": ["-12.9744658", "-38.5131887"]
+
+        },
+
+        {
+
+            "code_muni": "230440",
+
+            "name_muni": "Fortaleza",
+
+            "name_state": "CE",
+
+            "display_text": "Praça do Ferreira",
+
+            "complement": "conhecida como \"coração da cidade\"",
+
+            "input_point": ["-3.7277894", "-38.5276207"]
+
+        },
+
+        # NORTE
+
+        {
+
+            "code_muni": "130260",
+
+            "name_muni": "Manaus",
+
+            "name_state": "AM",
+
+            "display_text": "Teatro Amazonas",
+
+            "complement": "centro cultural de fama internacional",
+
+            "input_point": ["-3.1301977", "-60.0232912"]
+
+        },
+
+        {
+
+            "code_muni": "150140",
+
+            "name_muni": "Belém",
+
+            "name_state": "PA",
+
+            "display_text": "Mercado Ver-o-Peso",
+
+            "complement": "na margem da Baía do Guajará",
+
+            "input_point": ["-1.4525862","-48.5038115"]
 
         },
 
